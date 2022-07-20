400fe6854eed41261be7ea1b94eec48736daef09
David Bochan <11370593+dabo248@users.noreply.github.com>
2022-07-20 21:20:54 +0000

    refactor: simplify sample program

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,5 @@
-// import { readFile, readFileSync } from 'fs'
-// import { resolve } from 'path'
-
-// console.log(readFileSync.toString())
-// console.log(readFile.toString())
-
-// console.log(resolve.toString()
-// )
-
-console.log('Hello World')
-
-await Promise.resolve(true)
-
 const greet = (name: string) => {
-    const list = [4, 5, 6]
-
-    for (const i in list) {
-        console.log(i) // "0", "1", "2",
-    }
-    console.log('Hello ' + name)
+    console.log('Hello, ' + name + '!')
 }
 
-greet('David')
-
-const number = 12
-console.log(number)
+greet('world')
